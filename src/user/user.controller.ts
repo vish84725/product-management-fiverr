9cb07fcab63c53e99c88b7c880b86bc6e717b73c
Visal <vish847255@gmail.com>
2021-07-08 01:38:41 +0530

    add update endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Put, UseGuards } from '@nestjs/common';
 import { ApiUseTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { UserService } from './user.service';
 import { AuthService } from '../utils/auth.service';
 import { UtilService } from '../utils/util.service';
-import { UserCreateDTO, ResponseLogin, LoginDTO } from './user.model';
+import { UserCreateDTO, ResponseLogin, LoginDTO, UserUpdateDTO, UsersDTO } from './user.model';
 import { ResponseSuccessMessage, ResponseBadRequestMessage, ResponseErrorMessage, CommonResponseModel, ResponseMessage } from '../utils/app.model';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from '../utils/jwt.strategy';
 
 @Controller('users')
 @ApiUseTags('Users')
@@ -37,6 +39,42 @@ public async registerNewUser(@Body() userData: UserCreateDTO): Promise<CommonRes
             return this.utilService.successResponseMsg(ResponseMessage.USER_REGISTERED);
         }
         else this.utilService.badRequest(ResponseMessage.SOMETHING_WENT_WRONG);
+    } catch (e) {
+        throw e;
+        //this.utilService.errorResponse(e);
+    }
+}
+
+@Put('')
+@ApiOperation({ title: 'Update user' })
+@UseGuards(AuthGuard('jwt'))
+@ApiResponse({ status: 200, description: 'Success message', type: ResponseSuccessMessage })
+@ApiResponse({ status: 400, description: 'Bad request message', type: ResponseBadRequestMessage })
+@ApiResponse({ status: 404, description: 'Unauthorized or Not found', type: ResponseErrorMessage })
+public async updateUser(@GetUser() loginUser: UsersDTO,@Body() userData: UserUpdateDTO): Promise<CommonResponseModel> {
+    try {
+        const mobileNumber = this.utilService.convertToNumber(userData.mobileNumber);
+
+        if (mobileNumber == 0) this.utilService.badRequest(ResponseMessage.REQUIRED_VALID_MOBILE_NUMBER);
+
+        const checkUser = await this.userService.findUserByEmail(userData.email);
+
+        if(checkUser){
+            const isMobileAlreadyHasUser = await this.userService.findUserByMobile(checkUser._id,userData.mobileNumber);
+            console.log('u',isMobileAlreadyHasUser);
+            if(isMobileAlreadyHasUser){
+                this.utilService.badRequest(ResponseMessage.USER_MOBILE_ALREADY_EXIST);
+            }
+            const updatedData = await this.userService.updateUser(checkUser._id,userData);
+			if (updatedData){
+                return this.utilService.successResponseMsg(ResponseMessage.USER_UPDATED_SUCCESSFULLY);
+            }else{
+                this.utilService.badRequest(ResponseMessage.SOMETHING_WENT_WRONG);
+            } 
+
+        }else{
+            this.utilService.badRequest(ResponseMessage.USER_EMAIL_DOESNT_EXIST)
+        }     
     } catch (e) {
         throw e;
         //this.utilService.errorResponse(e);
